470449fb788b7e794e3c91c8f3f8cc45d6b21883
Noor El-Din Walid <noureldin.walid@robustastudio.com>
2025-01-30 13:30:43 +0200

    Adding colors and some assets

---
+++ Sources/Baytoti-Design-System/Colors.swift
@@ -0,0 +1,27 @@
+//
+//  Colors.swift
+//  Baytoti-Design-System
+//
+//  Created by Noor El-Din Walid on 30/01/2025.
+//
+
+import SwiftUI
+
+public enum Colors {
+    static let primaryColor100 = Color(red: 1, green: 0.62, blue: 0.11)
+    static let primaryColor80 = Color(red: 1, green: 0.69, blue: 0.29)
+    static let primaryColor60 = Color(red: 1, green: 0.77, blue: 0.46)
+    static let primaryColor40 = Color(red: 1, green: 0.85, blue: 0.64)
+    static let primaryColor20 = Color(red: 1, green: 0.92, blue: 0.82)
+    
+    static let mainRed100 = Color(red: 0.95, green: 0.15, blue: 0.21)
+    static let mainRed80 = Color(red: 0.96, green: 0.32, blue: 0.36)
+    static let mainRed60 = Color(red: 0.97, green: 0.49, blue: 0.53)
+    static let mainRed40 = Color(red: 0.98, green: 0.66, blue: 0.68)
+    static let mainRed20 = Color(red: 0.99, green: 0.83, blue: 0.84)
+    
+    static let secondaryLightGreen = Color(red: 0, green: 0.69, blue: 0.64)
+    static let secondaryBlue = Color(red: 0, green: 0.45, blue: 0.78)
+    static let secondaryPurple = Color(red: 0.45, green: 0.14, blue: 0.42)
+    static let secondaryGreen = Color(red: 0, green: 0.52, blue: 0.25)
+}
