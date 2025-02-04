e757791f975a1ca54874f97f83e46df9531a84bc
Noor El-Din Walid <noureldin.walid@robustastudio.com>
2025-02-04 19:05:44 +0200

    test1

---
+++ Sources/Baytoti-Design-System/Image+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  ImageProvider.swift
+//  Baytoti-Design-System
+//
+//  Created by Noor El-Din Walid on 04/02/2025.
+//
+
+import SwiftUI
+
+public extension Image {
+    static func designImage(_ name: String) -> Image {
+        Image(name, bundle: .module)
+    }
+}
